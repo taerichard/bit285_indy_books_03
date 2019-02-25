47b21455948c3c46261f6cb9dcd76907b92ca7d2
Brian Bansenauer <brianb@cascadia.edu>
2019-02-24 20:48:59 -0800

    intial

---
+++ IndyBooks/Controllers/AdminController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IndyBooks.Models;
+using IndyBooks.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndyBooks.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IndyBooksDataContext _db;
+        public AdminController(IndyBooksDataContext db) { _db = db; }
+
+        /***
+         * CREATE
+         */
+        [HttpGet]
+        public IActionResult CreateBook()
+        {
+            return View("AddBook");
+        }
+        [HttpPost]
+        public IActionResult CreateBook(AddBookViewModel newBook)
+        {
+            //TODO: Build the Author from either the AuthorId (if it is > 0), or the Name
+            Writer author;
+            
+
+            //TODO: Build the Book using the newBook data and your newly created author.
+            Book book; 
+
+            //TODO: Add author and book to their DbSets; SaveChanges
+           
+
+            //Shows the new book using the Index View 
+            return RedirectToAction("Index");
+        }
+        /***
+         * READ       
+         */
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //TODO: Use lambda methods on _db.Books as described by the variable name
+            var allBooksWithAuthorsOrderedbySKU = _db.Books;
+            return View("SearchResults", allBooksWithAuthorsOrderedbySKU);
+        }
+        /***
+         * UPDATE
+         */
+         //TODO: BONUS - Write an method that take a book id, and loads the book data in to the AddBook View
+
+        /***
+         * DELETE
+         */
+        [HttpGet]
+        public IActionResult DeleteBook(long id)
+        {
+            //TODO: Remove the Book associated with the given id number; Save Changes
+
+
+            return RedirectToAction("Search");
+        }
+
+        [HttpGet]
+        public IActionResult Search() { return View(); }
+        [HttpPost]
+        public IActionResult Search(SearchViewModel search)
+        {
+            //Full Collection Search
+            IQueryable<Book> foundBooks = _db.Books; // start with entire collection
+
+            //Partial Title Search
+            if (search.Title != null)
+            {
+                foundBooks = foundBooks
+                            .Where(b => b.Title.Contains(search.Title))
+                            .OrderBy(b => b.Title)
+                            ;
+            }
+
+            //Author's Last Name Search
+            if (search.AuthorLastName != null)
+            {
+                //Use the Name property of the Book's Author entity
+                foundBooks = foundBooks
+                            .Include(b => b.Author)
+                            .Where(b => b.Author.Name.EndsWith(search.AuthorLastName, StringComparison.CurrentCulture))
+                            ;
+            }
+            //Priced Between Search (min and max price entered)
+            if (search.MinPrice > 0 && search.MaxPrice > 0)
+            {
+                foundBooks = foundBooks
+                            .Where(b => b.Price >= search.MinPrice && b.Price <= search.MaxPrice)
+                            .Select(b => new Book { Author = b.Author })
+                            .Distinct()
+                            ;
+            }
+            //Highest Priced Book Search (only max price entered)
+            if (search.MinPrice == 0 && search.MaxPrice > 0)
+            {
+                decimal max = _db.Books.Max(b => b.Price);
+                foundBooks = foundBooks
+                            .Where(b => b.Price == max)
+                            ;
+            }
+            //Composite Search Results
+            return View("SearchResults", foundBooks.Include(b => b.Author));
+        }
+
+    }
+}
